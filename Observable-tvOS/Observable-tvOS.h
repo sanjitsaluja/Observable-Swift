b12e430567bf465a4d2d9a55f59d54ca917c6619
Sanjit Saluja <sanjits@zillow.com>
2016-01-08 19:26:34 +0530

    TVOS

---
+++ Observable-tvOS/Observable-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Observable-tvOS.h
+//  Observable-tvOS
+//
+//  Created by Sanjit Saluja on 1/8/16.
+//  Copyright © 2016 Leszek Ślażyński. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Observable-tvOS.
+FOUNDATION_EXPORT double Observable_tvOSVersionNumber;
+
+//! Project version string for Observable-tvOS.
+FOUNDATION_EXPORT const unsigned char Observable_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Observable_tvOS/PublicHeader.h>
+
+
